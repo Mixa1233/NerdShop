67dd62922e0647c16a13f4b0baadf78e9e7fb86c
Mixa1233 <mihajlomarinkovic8@gmail.com>
2024-01-05 11:51:32 +0100

    Footer and Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <h1>
+    <>
+    <Header/>
+    <main className='py-3'>
+      <Container>
       Welcome to NerdShop
-    </h1>
+      </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
