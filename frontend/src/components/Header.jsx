8e6ea4e3631305677e9fed25c34985fc3123a5a5
Mixa1233 <mihajlomarinkovic8@gmail.com>
2024-01-20 13:05:51 +0100

    7th commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,43 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart)
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-<Container>
-  <LinkContainer to='/'>
-<Navbar.Brand>NerdShop</Navbar.Brand>
-</LinkContainer>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>NerdShop</Navbar.Brand>
+          </LinkContainer>
 
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg='sucess' style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
+                </Nav.Link>
+              </LinkContainer>
 
-<Navbar.Toggle aria-controls='basic-navbar-nav' />
-<Navbar.Collapse id='basic-navbar-nav'>
-<Nav className='ms-auto'>
-
-<LinkContainer to='/cart'>
-    <Nav.Link><FaShoppingCart/>Cart</Nav.Link>
-    </LinkContainer>
-
-    <LinkContainer to='/login'>
-    <Nav.Link><FaUser/>Sign In</Nav.Link>
-    </LinkContainer>
-</Nav>
-</Navbar.Collapse>
-</Container>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaUser />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   )
